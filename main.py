83f8e0258bc2317c17ffa183d129df427098e990
Yousef Issa <yousef@u.northwestern.edu>
2018-03-15 21:59:47 -0500

    updated to show sizes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import json
 import logging
 import requests
+from bs4 import BeautifulSoup
 
 def logger_setup(): 
     logger = logging.getLogger('discord')
@@ -35,33 +36,52 @@ async def on_message(message):
 
         response = requests.post(url=json_data['api_url'], headers=headers, params=params, json=data)
         output = json.loads(response.text)
+        try:
+            sizes = get_size_info(output['hits'])
+            deadstock_sold = output['hits'][0]['deadstock_sold']
+            highest_ask = output['facets_stats']['lowest_ask']['max']
+            highest_bid = output['hits'][0]['highest_bid']
+            image = output['hits'][0]['media']['imageUrl']
+            last_sale = output['hits'][0]['last_sale']
+            lowest_ask = output['hits'][0]['lowest_ask']
+            name = output['hits'][0]['name']
+            # retail = output['hits'][0]['traits'][2]['value']
+            sales_last_72 = output['hits'][0]['sales_last_72']
+            # style = output['hits'][0]['style_id']
+            url = 'https://stockx.com/' + output['hits'][0]['url']
 
-        deadstock_sold = output['hits'][0]['deadstock_sold']
-        highest_ask = output['facets_stats']['lowest_ask']['max']
-        highest_bid = output['hits'][0]['highest_bid']
-        image = output['hits'][0]['media']['imageUrl']
-        last_sale = output['hits'][0]['last_sale']
-        lowest_ask = output['hits'][0]['lowest_ask']
-        name = output['hits'][0]['name']
-        # retail = output['hits'][0]['traits'][2]['value']
-        sales_last_72 = output['hits'][0]['sales_last_72']
-        # style = output['hits'][0]['style_id']
-        url = 'https://stockx.com/' + output['hits'][0]['url']
+            embed = discord.Embed(color=4500277)
+            embed.set_thumbnail(url=image)
+            embed.add_field(name="Product Name", value="[{}]({})".format(name, url), inline=False)
+            # embed.add_field(name="Product Style ID", value="{}".format(style), inline=False)
+            # embed.add_field(name="Retail", value="${}".format(retail), inline=False)
+            embed.add_field(name="Last Sale", value="${}, {}".format(last_sale, sizes['last_sale']), inline=True)
+            embed.add_field(name="Highest Bid", value="${}, {}".format(highest_bid, sizes['highest_bid_size']), inline=True)
+            embed.add_field(name="Lowest Ask", value="${}, {}".format(lowest_ask, sizes['lowest_ask_size']), inline=True)
+            embed.add_field(name="Highest Ask", value="${}".format(highest_ask), inline=True)
+            embed.add_field(name="Units Sold in Last 72 Hrs", value="{}".format(sales_last_72), inline=True)
+            embed.add_field(name="Total Units Sold", value="{}".format(deadstock_sold), inline=True)
+            await client.send_message(message.channel, embed=embed)
+        except IndexError:
+            await client.send_message(message.channel, 'Error getting info from stockX.')
 
-        embed = discord.Embed(color=4500277)
-        embed.set_thumbnail(url=image)
-        embed.add_field(name="Product Name", value="[{}]({})".format(name, url), inline=False)
-        # embed.add_field(name="Product Style ID", value="{}".format(style), inline=False)
-        # embed.add_field(name="Retail", value="${}".format(retail), inline=False)
-        embed.add_field(name="Last Sale", value="${}".format(last_sale), inline=True)
-        embed.add_field(name="Highest Bid", value="${}".format(highest_bid), inline=True)
-        embed.add_field(name="Lowest Ask", value="${}".format(lowest_ask), inline=True)
-        embed.add_field(name="Highest Ask", value="${}".format(highest_ask), inline=True)
-        embed.add_field(name="Units Sold in Last 72 Hrs", value="{}".format(sales_last_72), inline=True)
-        embed.add_field(name="Total Units Sold", value="{}".format(deadstock_sold), inline=True)
-
-        await client.send_message(message.channel, embed=embed)
-
+def get_size_info(hits):
+    '''
+        Takes a hits json as an input
+    '''
+    try:
+        soup = BeautifulSoup(price_get.text, 'html.parser')   
+        return {
+            'last_sale' : soup.find('div', {'class': 'last-sale-block'}).find('span', {'class':'bid-ask-sizes'}).text,
+            'lowest_ask_size': soup.find('div', {'class': 'bid bid-button-b'}).find('span', {'class':'bid-ask-sizes'}).text,
+            'highest_bid_size': soup.find('div', {'class': 'ask ask-button-b'}).find('span', {'class':'bid-ask-sizes'}).text 
+        }
+    except:
+        return {
+            'last_sale' : '',
+            'lowest_ask_size': '',
+            'highest_bid_size': '' 
+        }
 
 if __name__ == '__main__':
     logger_setup()
